d0c35b4c35972d6577b921c016ae1d09d7e77ab2
3s7an <tristan.prekop13@gmail.com>
2024-09-18 20:59:43 +0200

    DASHBOARD - DESIGN

--- eshop/resources/views/dashboard.blade.php
+++ eshop/resources/views/dashboard.blade.php
@@ -11,9 +11,14 @@
     <!-- Produkty -->
     <div class="w-4/5">
 
-        <h1 class="text-center">Zlatnictvo</h1>
-        <p class="text-center">Nejaký trápny slogan</p>
-        <h1 class="text-center my-9 font-bold text-4xl font-popins">Produkty :</h1>
+        <div class="bg-gray-100 flex flex-col items-center justify-center p-8 rounded-lg shadow-md mt-10">
+            <h1 class="text-gray-800 font-lobster text-7xl my-10">Zlatníctvo</h1>
+            <p class="text-gray-700">Nejaký trápny slogan</p>
+        </div>
+
+        <hr>
+
+        <h1 class="text-center my-20 font-bold text-4xl font-popins">Produkty :</h1>
 
 
         <div class="flex flex-wrap">
